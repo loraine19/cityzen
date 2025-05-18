309e06bf6ac4736732ed33e2ec7c3e3ae8cc48e5
lorainep <lou.hoffmann@gmail.com>
2025-05-18 11:16:08 +0200

    up address

--- src/presenter/components/shared/myInfos/MyInfosPage.tsx
+++ src/presenter/components/shared/myInfos/MyInfosPage.tsx
@@ -86,7 +86,10 @@ export default function MyInfosPage() {
 
 
 
-    useEffect(() => { address && (formik.values.Address = address) }, [address])
+    useEffect(() => {
+        if (address) formik.values.Address = address
+        else navigate("/profile/create")
+    }, [address])
 
     return (
         <div className="Body gray flex">
