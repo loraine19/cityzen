012dbfcc3429ab36af13d7d1fa6992052df1567c
lorainep <lou.hoffmann@gmail.com>
2025-07-03 14:06:12 +0200

    css

--- src/presenter/components/common/NavBarBottom.tsx
+++ src/presenter/components/common/NavBarBottom.tsx
@@ -87,11 +87,11 @@ const NavBarBottom: React.FC<NavBarBottomProps> = ({ addBtn = false }) => {
                 className={
                     ((hideNavBottom && navBottom) ? 'transform-y[-100%] hidden opacity-0 anim z-0' : '!my-0 ') +
                     ((hideNavBottom && !navBottom) ? 'hidden ' : '') +
-                    ((navBottom) ? `items-center pb-2 bottom-0 transform-y-0 opacity-100 anim ${color}BG backdropBlur w-respXl rounded-full justify-center` : 'pt-2 fixed left-1/2 -translate-x-1/2 -top-3 lg:-top-2.5 z-0 md:scale-[0.80] scale-[0.72] w-[calc(100%-8rem)] lg:!max-w-[calc(1000px-4rem)] -ml-5 items-start !justify-start') +
+                    ((navBottom) ? `items-center pb-2 bottom-0 transform-y-0 opacity-100 anim ${color}BG backdropBlur w-respXl rounded-full justify-center` : 'pt-2 fixed left-1/2 -translate-x-1/2 -top-2.5  z-0 md:scale-[0.80] scale-[0.72] w-[calc(100%-8rem)] lg:!max-w-[calc(1000px-4rem)] -ml-5 items-start !justify-start') +
                     ` flex gap-6`
                 }>
 
-                <Navbar className={`${navBottom ? 'min-w-max !max-w-[calc(100vw-8rem)] w-full shadow-md !bg-white/95 border border-blue-gray-100/50' : ' overflow-x-auto !pt-0.5 lg:!pt-0 lg:overflow-hidden !max-w-[calc(100%-4rem)]'} -ml-4   flex rounded-full h-full items-center p-0 `}>
+                <Navbar className={`${navBottom ? 'min-w-max !max-w-[calc(100vw-8.5rem)] w-full shadow-md !bg-white/95 border border-blue-gray-100/50' : ' overflow-x-auto  lg:!pt-0 lg:overflow-hidden !max-w-[calc(100%-4rem)]'} -ml-4   flex rounded-full h-full items-center p-0 `}>
                     <div className={`${navBottom ? "flex-row" : 'flex-row-reverse'} w-full min-w-max h-full relative`}>
                         <ul className={`flex !max-w-[calc(100vw-8rem)] overflow-auto flex-row w-full  rounded-full justify-between h-full gap-auto`}>
                             {navItems.map(({ to, icon, label, color, col }: NavItem, index) => (
